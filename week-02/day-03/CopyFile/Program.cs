f7cbb7fe58b409ffe634cf3f5ea303cbad677b56
Ondrej Sebl <62696517+donkeyjot@users.noreply.github.com>
2020-09-23 17:08:27 +0200

    Copying files

---
+++ week-02/day-03/CopyFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CopyFile("tetx.txt", "copy.txt"));
+        }
+
+        private static bool CopyFile(string sourceFile, string newFile)
+        {
+            try
+            {
+                string[] allLines = File.ReadAllLines(sourceFile);
+                File.WriteAllLines(newFile, allLines);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
