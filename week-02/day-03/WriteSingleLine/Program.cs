100913ea2ec97d59c42e74e1490ca21cdb38f2b7
Ondrej Sebl <62696517+donkeyjot@users.noreply.github.com>
2020-09-23 16:17:16 +0200

    Writing names and solving for problems

---
+++ week-02/day-03/WriteSingleLine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+// Write a function that is able to manipulate a file
+// By writing your name into it as a single line
+// The file should be named "my-file.txt"
+// In case the program is unable to write the file,
+// It should print the following error message: "Unable to write file: my-file.txt"
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SaveName("text.txt");
+            SaveName("text.txt");
+
+            Console.WriteLine(File.ReadAllText("text.txt"));
+        }
+        public static void SaveName(string filename)
+        {
+            Console.WriteLine("What is your name?");
+
+            List<string> names = new List<string>()
+            {
+                {Console.ReadLine() }
+            };
+
+            try
+            {
+                File.AppendAllLines(filename, names);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: " + filename);
+            }
+        }
+    }
+
+}
